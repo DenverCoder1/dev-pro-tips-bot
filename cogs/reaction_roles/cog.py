5f70a7b34f3fdcca679a9402a0e99fe7f16c217a
Jonah Lawrence <jonah@freshidea.com>
2021-07-01 23:07:17 +0300

    add member role

--- cogs/reaction_roles/cog.py
+++ cogs/reaction_roles/cog.py
@@ -13,6 +13,7 @@ class ReactionRolesCog(commands.Cog, name="Reaction Roles"):
     @commands.Cog.listener()
     async def on_ready(self):
         guild = self.bot.get_guild(config.GUILD_ID)
+        self.member_role = guild.get_role(config.MEMBER_ROLE_ID)
         self.unassigned_role = guild.get_role(config.UNASSIGNED_ROLE_ID)
 
     @commands.Cog.listener()
@@ -21,6 +22,8 @@ class ReactionRolesCog(commands.Cog, name="Reaction Roles"):
         role_assignment = self.reaction_handler.validate_reaction(payload)
         if role_assignment is not None:
             await role_assignment.member.add_roles(role_assignment.role)
+            # add member role
+            await role_assignment.member.add_roles(self.member_role)
             # remove unassigned role
             await role_assignment.member.remove_roles(self.unassigned_role)
 
