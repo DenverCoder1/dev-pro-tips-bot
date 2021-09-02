ed1e90f75c8c63aa6e745520bfd90de024172e67
Jonah Lawrence <jonah@freshidea.com>
2021-09-03 01:03:07 +0300

    added confirm button

---
+++ cogs/button_roles/confirm_view.py
@@ -0,0 +1,20 @@
+from .role_view import RoleView
+from utils.utils import custom_id
+import nextcord
+import config
+
+VIEW_NAME = "ConfirmView"
+
+
+class ConfirmView(RoleView):
+    def __init__(self):
+        super().__init__()
+
+    @nextcord.ui.button(
+        label="Confirm",
+        emoji="👍",
+        style=nextcord.ButtonStyle.green,
+        custom_id=custom_id(VIEW_NAME, config.MEMBER_ROLE_ID),
+    )
+    async def subscriber_button(self, button, interaction):
+        await self.handle_click(button, interaction)
